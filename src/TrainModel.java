250866d5ed9e9c8a97afdb6b06c2b6f60de507f3
zggit <edwardzhangchina@hotmail.com>
2014-12-09 21:34:14 -0500

    New Update

--- src/TrainModel.java
+++ src/TrainModel.java
@@ -7,6 +7,7 @@ public class TrainModel extends javax.swing.JFrame {
     String nextStation;
     String failure;
     
+    boolean approachingStation = false;
     boolean atStation = false;              //true means at Station and false means not
     boolean underground = false;            //true means underground and false means not
     boolean lightStatus = false;            //true means on and false mean close
@@ -16,6 +17,9 @@ public class TrainModel extends javax.swing.JFrame {
     boolean engineFailure = false;          //true means failed engine and false means okay
     boolean brakeFailure = false;           //true means failed brakes and false means okay
     boolean signalPickupFailure = false;    //true means failed antenna and false means okay
+    boolean inputBrakeFailure = false;
+    boolean inputEngineFailure = false;
+    boolean inputSignalFailure = false;
     
     double passengerMass = 80.7;//Kilograms
     int passengerCount;
@@ -67,6 +71,10 @@ public class TrainModel extends javax.swing.JFrame {
     /////////////////////////////////////////
     //Getter, Setter, & Calculation Functions
     /////////////////////////////////////////
+    
+    public void setApproachingStation(boolean a){
+        this.approachingStation = a;
+    }
     
     public void setAtStation(boolean s){
         this.atStation = s;
@@ -134,6 +142,8 @@ public class TrainModel extends javax.swing.JFrame {
             currentAcceleration = -1.2;     // m/sec^2
         else if(eBrake)
             currentAcceleration = -2.73;    // m/sec^2
+        else if(inputBrakeFailure)
+            currentAcceleration = 0;
         else
             currentAcceleration = currentForce / mass;
         
@@ -179,6 +189,13 @@ public class TrainModel extends javax.swing.JFrame {
         setPowerTF = new javax.swing.JTextField();
         currentPositionTF = new javax.swing.JTextField();
         currentSpeedTF = new javax.swing.JTextField();
+        engineFailureButton = new javax.swing.JToggleButton();
+        brakeFailureButton = new javax.swing.JToggleButton();
+        signalFailureButton = new javax.swing.JToggleButton();
+        jLabel2 = new javax.swing.JLabel();
+        engineFailureDisplay = new javax.swing.JTextField();
+        brakeFailureDisplay = new javax.swing.JTextField();
+        signalFailureDisplay = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -188,7 +205,7 @@ public class TrainModel extends javax.swing.JFrame {
 
         currentPosisiontLB.setText("Current Position:");
 
-        setSpeedLB.setText("Set Power");
+        setSpeedLB.setText("Set Power:");
 
         setPowerTF.setText("Set Power");
 
@@ -196,26 +213,69 @@ public class TrainModel extends javax.swing.JFrame {
 
         currentSpeedTF.setText("Current Speed");
 
+        engineFailureButton.setText("Engine Failure");
+        engineFailureButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                engineFailureButtonActionPerformed(evt);
+            }
+        });
+
+        brakeFailureButton.setText("Brake Failure");
+        brakeFailureButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                brakeFailureButtonActionPerformed(evt);
+            }
+        });
+
+        signalFailureButton.setText("Signal Failure");
+        signalFailureButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                signalFailureButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Current Failure: ");
+
+        engineFailureDisplay.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                engineFailureDisplayActionPerformed(evt);
+            }
+        });
+
+        signalFailureDisplay.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                signalFailureDisplayActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(35, 35, 35)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(currentPosisiontLB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(currentSpeedLB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(setSpeedLB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(setPowerTF)
-                    .addComponent(currentPositionTF)
-                    .addComponent(currentSpeedTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(155, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(55, 55, 55)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(currentPosisiontLB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(currentSpeedLB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(setSpeedLB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel2)
+                    .addComponent(engineFailureDisplay)
+                    .addComponent(brakeFailureDisplay)
+                    .addComponent(signalFailureDisplay))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(signalFailureButton)
+                    .addComponent(engineFailureButton)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(setPowerTF)
+                        .addComponent(currentPositionTF)
+                        .addComponent(currentSpeedTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(brakeFailureButton))
+                .addContainerGap(232, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -234,11 +294,73 @@ public class TrainModel extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(setSpeedLB)
                     .addComponent(setPowerTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(307, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addGap(6, 6, 6)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(engineFailureButton)
+                    .addComponent(engineFailureDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(brakeFailureButton)
+                    .addComponent(brakeFailureDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(signalFailureButton)
+                    .addComponent(signalFailureDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(147, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void brakeFailureButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_brakeFailureButtonActionPerformed
+        // TODO add your handling code here:
+        inputBrakeFailure = brakeFailureButton.isSelected();
+        if(inputBrakeFailure){
+            brakeFailureButton.setText("Engine Failure");
+            brakeFailureDisplay.setText("Open");
+        }
+        else{
+            brakeFailureButton.setText("Fix Failure");
+            brakeFailureDisplay.setText("Closed");
+        }
+    }//GEN-LAST:event_brakeFailureButtonActionPerformed
+
+    private void engineFailureDisplayActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_engineFailureDisplayActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_engineFailureDisplayActionPerformed
+
+    private void signalFailureDisplayActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_signalFailureDisplayActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_signalFailureDisplayActionPerformed
+
+    private void engineFailureButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_engineFailureButtonActionPerformed
+        // TODO add your handling code here:
+        inputEngineFailure = engineFailureButton.isSelected();
+        if(inputEngineFailure){
+            engineFailureButton.setText("Engine Failure");
+            engineFailureDisplay.setText("Open");
+        }
+        else{
+            engineFailureButton.setText("Fix Failure");
+            engineFailureDisplay.setText("Closed");
+        }
+    }//GEN-LAST:event_engineFailureButtonActionPerformed
+
+    private void signalFailureButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_signalFailureButtonActionPerformed
+        // TODO add your handling code here:
+        inputSignalFailure = signalFailureButton.isSelected();
+        if(inputSignalFailure){
+            signalFailureButton.setText("Engine Failure");
+            signalFailureDisplay.setText("Open");
+        }
+        else{
+            signalFailureButton.setText("Fix Failure");
+            signalFailureDisplay.setText("Closed");
+        }
+    }//GEN-LAST:event_signalFailureButtonActionPerformed
 
     
     
@@ -275,12 +397,19 @@ public class TrainModel extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton brakeFailureButton;
+    private javax.swing.JTextField brakeFailureDisplay;
     private javax.swing.JLabel currentPosisiontLB;
     private javax.swing.JTextField currentPositionTF;
     private javax.swing.JLabel currentSpeedLB;
     private javax.swing.JTextField currentSpeedTF;
+    private javax.swing.JToggleButton engineFailureButton;
+    private javax.swing.JTextField engineFailureDisplay;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JTextField setPowerTF;
     private javax.swing.JLabel setSpeedLB;
+    private javax.swing.JToggleButton signalFailureButton;
+    private javax.swing.JTextField signalFailureDisplay;
     // End of variables declaration//GEN-END:variables
 }
