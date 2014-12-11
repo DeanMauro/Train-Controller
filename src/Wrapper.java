995a0f97421485c6fc41cd14a0810fca939a8e6f
coelabs <coelabs@COELABS05.engr.nt.pitt.edu>
2014-12-11 06:26:50 -0500

    number passengers

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -27,6 +27,8 @@ class Wrapper {
         protected static int numberOfTrains = 0;
         
         protected static Random randomGen = new Random();
+        
+        public static int numPassengers = 0;
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * *
  * MAIN 
@@ -185,6 +187,10 @@ class Wrapper {
                           ad.setAdToTaco();
                           ad.setVisible(true);
                       }
+                  }
+                  if(trackModelInterface.getTrackModel().getBlockTrainIsOn(currentTrain.getID()).isStation())
+                  {
+                      numPassengers = randomGen.nextInt(222);
                   }
                   
               }
